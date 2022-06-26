f5f0d25509470435421c897bf742310d4689dde2
fuad-bassam <f.man.x99@gmail.com>
2022-06-26 18:04:39 +0300

    final1

--- FastMarket/FastMarket/Pages/CartDetail/EditCartItem.cshtml.cs
+++ FastMarket/FastMarket/Pages/CartDetail/EditCartItem.cshtml.cs
@@ -1,54 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using FastMarket.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
+
 namespace FastMarket.Pages.CartDetail
 {
-    public class IndexModel : PageModel
+    public class EditCartItemModel : PageModel
     {
-        [BindProperty]
-        public int Count { get; set; }
-        [BindProperty]
-        public Product product { get; set; }
         [BindProperty]
         public List<Product> ListProduct { get; set; }
 
         [BindProperty]
-        public decimal TotalPrice { get; set; }
-
-
-        public void OnGet()
+        public Product EditedProduct { get; set; }
+        public void OnGet(int id)
         {
-            if (HttpContext.Request.Cookies["Count"] != null)
-            {
-                Count = int.Parse(HttpContext.Request.Cookies["Count"]);
 
-                ListProduct = JsonConvert.DeserializeObject<List<Product>>
-                        (HttpContext.Request.Cookies["ProductObject"]);
-                foreach (Product item in ListProduct)
-                {
-                    // * Amount
-                    TotalPrice += item.Price;
-                }
-            }
+            ListProduct = JsonConvert.DeserializeObject<List<Product>>
+                      (HttpContext.Request.Cookies["ProductObject"]);
+            EditedProduct= ListProduct.Find(x => x.Id == id);
+
+
         }
         public async void OnPostAsync(int id)
         {
             ListProduct = JsonConvert.DeserializeObject<List<Product>>
                       (HttpContext.Request.Cookies["ProductObject"]);
-            ListProduct.Remove(ListProduct.Find(x=>x.Id ==id));
+            ListProduct.Remove(ListProduct.Find(x => x.Id == id));
             var JsonFile = JsonConvert.SerializeObject(ListProduct);
             CookieOptions cookieOptions = new CookieOptions();
             cookieOptions.Expires = new System.DateTimeOffset(DateTime.Now.AddDays(7));
             HttpContext.Response.Cookies.Append("Count", ListProduct.Count.ToString(), cookieOptions);
             HttpContext.Response.Cookies.Append("ProductObject", JsonFile, cookieOptions);
             // await OnGet();
-          
+
             // alert "item deleted"
             Page();
         }
+
     }
 }
