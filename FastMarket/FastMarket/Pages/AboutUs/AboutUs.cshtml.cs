79209e5507b24c25decf29582871b2cd60759519
Hanan-Nathem-Saadeh <hanan.nathem111@gmail.com>
2022-06-28 13:00:06 +0300

    final2

---
+++ FastMarket/FastMarket/Pages/AboutUs/AboutUs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FastMarket.Pages.AboutUs
+{
+    public class AboutUsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
